ecaac7dac7c9c1a9fed742ec3adbdb2856a05d20
whatsquackin@hotmail.com <whatsquackin@hotmail.com>
2020-06-22 16:30:37 -0400

    first push

---
+++ src/main/java/com/win/user/user/User.java
@@ -0,0 +1,58 @@
+package com.win.user.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User { 
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Name is mandatory")
+    private String email;
+
+	public User(@NotBlank(message = "Name is mandatory") String name,
+			@NotBlank(message = "Name is mandatory") String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public User(){
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+    
+    
+}
